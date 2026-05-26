9b6708b7492c33aff5ee147a35f14ca40e84d7ce
Josue-Zapata-v <zjosue775@gmail.com>
2026-05-26 02:08:05 -0500

    second commit

--- backend/src/db.js
+++ backend/src/db.js
@@ -2,7 +2,7 @@ import pg from 'pg';
 const { Pool } = pg;
 
 export const pool = new Pool({
-  host: process.env.DB_HOST || 'db',
+  host: process.env.DB_HOST || 'localhost',
   port: process.env.DB_PORT || 5432,
   database: process.env.DB_NAME || 'contactsdb',
   user: process.env.DB_USER || 'postgres',
